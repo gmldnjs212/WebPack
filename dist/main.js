57dee314460936d209c278a4c8fb948e36f6fdec
heee1 <p1xell@kakao.com>
2023-07-19 22:29:10 +0900

    230719_2229

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,3 @@
+import '../css/main.css' // css파일만 가져와서 main.js에 연결하는 것
+
 console.log("Webpack!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,49 @@
 // import 
 const path = require('path')
-
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 // export
 module.exports = {
-  // parcel index.html => parcel bundler의 진입점을 index.html로 설정한 것
-  // webpack은 html파일이 아닌 js파일을 진입점으로 설정한다
 
   // 파일을 읽어들이기 시작하는 진입점 설정
-  entry: './js/main.js', // == parcel main.js
+  entry: './js/main.js',
 
   // 결과물(번들)을 반환하는 설정
   output: {
-      // path는 node.js에서 요구하는 절대경로를 필요로한다
     // path: path.resolve(__dirname, 'dist'), 
-      // __dirname : 현재파일이 있는 그 경로를 지칭
-      // path.resolve : 인자를 두개 받으며, 두개의 인자를 합쳐주는 역할을 수행
     // filename: 'main.js',
-    clean: true // 기존에 만들었던 파일은 제거하고 새롭게 생성하는 옵션
+    clean: true 
+  },
 
-    // path와 filename을 주석처리하여도 webpack은 기본적으로
-    // dist폴더에 결과물을 생성하며 filename또한 entry에서 명시해주었기 때문에
-    // 반복하여 명시할 필요가 없다
+  module: {
+    rules: [
+      {
+        test: /\.css$/, 
+        // \.       = 마침표 그자체를 의미 
+        // /\.css$/ = .css로 끝나는 파일을 찾는 정규표현식
+        // ~$        = ~으로 끝나는..
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
 
-    // 단, 기본값이 아닌 다른 설정값으로 작동하길원한다면 수정해야 한다.
+  // 번들링 후 결과몰의 처리방식 등 다양한 플러그인들을 설정
+  plugins : [
+    new HtmlPlugin({
+      template : './index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from : 'static' }
+      ]
+    })
+  ],
+
+  devServer: {
+    host: 'localhost'
   }
 }
